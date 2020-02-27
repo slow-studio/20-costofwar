12624f62ce5981415a93a546abea1222821ed3f5
han_sh <shobhan.shah@network.rca.ac.uk>
2020-02-27 21:42:27 +0530

    start project. make two rectangles.

--- sketch.js
+++ sketch.js
@@ -2,15 +2,13 @@ function preload() {
 }
 
 function setup() {
-	let myCanvas = createCanvas(600, 400);
+	let myCanvas = createCanvas(600, 300);
 	myCanvas.parent("parent_to_p5_canvas");
 }
 
 function draw() {
-	if(mouseIsPressed) {
-		fill(0);
-	} else {
-		fill(255);
-	}
-  	ellipse(mouseX,mouseY,80,80);
+	fill(0);
+	rect(0,0,300,300);
+	fill(255);
+	rect(300,0,300,300);
 }
