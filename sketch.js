f808f8a35074945c11cf2f500e26a66cb9400906
han_sh <shobhan.shah@network.rca.ac.uk>
2020-03-27 23:39:44 +0530

    add dataset.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
 function preload() {
+	ckills = loadTable('data/coalition-kills.csv', 'csv');
+	tkills = loadTable('data/terrorist-kills.csv', 'csv');
+	week = loadTable('data/killDates.csv', 'csv');
 }
 
 function setup() {
 	noCanvas()
+	print("ckills = " + ckills.getColumnCount() )
+	print("tkills = " + tkills.getColumnCount() )
+	print("weeks = " + week.getColumnCount() )
 }
 
 function draw() {
