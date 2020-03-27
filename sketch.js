148cbb98445d0bce5a6c974a165cf26a16e4829b
han_sh <shobhan.shah@network.rca.ac.uk>
2020-03-27 23:38:01 +0530

    remove canvas. prepare to make the page's <div>s flicker.

--- sketch.js
+++ sketch.js
@@ -2,13 +2,8 @@ function preload() {
 }
 
 function setup() {
-	let myCanvas = createCanvas(600, 300);
-	myCanvas.parent("parent_to_p5_canvas");
+	noCanvas()
 }
 
 function draw() {
-	fill(0);
-	rect(0,0,300,300);
-	fill(255);
-	rect(300,0,300,300);
 }
